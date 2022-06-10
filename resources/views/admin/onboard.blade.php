eb6fb3cffffa85e060df1c13b6424d4e2615f65a
bi-suhas <bi.vpsuhas@gmail.com>
2022-06-10 16:22:05 +0530

    removed dummy text from user onboarding

--- resources/views/admin/onboard.blade.php
+++ resources/views/admin/onboard.blade.php
@@ -11,7 +11,7 @@
     <div class="uk-container uk-width-expand">
         <div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
             <h3 class="uk-card-title">Welcome to the world of opportunities</h3>
-            <p>Lorem ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <!-- <p>Lorem ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
             <ul uk-accordion>
                 <li class="{{ !$user->role ? 'uk-open' : '' }}">
                     <a class="uk-accordion-title acc_head" href="#" uk-tooltip="title: clich here to open your profile">#1 Your Profile</a>
