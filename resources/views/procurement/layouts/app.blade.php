d9b1fce93911e14cc1e4dd9ce959a0dae67467a8
bi-suhas <bi.vpsuhas@gmail.com>
2022-08-19 16:41:36 +0530

    merge conflict resolved

--- resources/views/procurement/layouts/app.blade.php
+++ resources/views/procurement/layouts/app.blade.php
@@ -237,7 +237,7 @@
     <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
 
-<<<<<<< HEAD
+
     <input id="fetch_parent_url" type="hidden" value="{{ route('getParent')}}">
     <input id="fetch_subcat_url" type="hidden" value="{{ route('getSubCategory')}}">
     <input id="fetch_region_url" type="hidden" value="{{ route('getRegion')}}">
